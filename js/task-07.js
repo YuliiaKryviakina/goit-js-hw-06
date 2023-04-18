aef341dc3f918de4a22b9c0bc37d577b216c191c
YuliiaKryviakina <117831702+YuliiaKryviakina@users.noreply.github.com>
2023-04-18 22:43:09 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputRange = document.querySelector('#font-size-control');
 const textOn = document.querySelector('#text');
 
-inputRange.addEventListener('change', valueFont => {
-    textOn.style.fontSize = '${valueFont.currentTarget.value }px'
+inputRange.addEventListener('input', e => {
+    textOn.style.fontSize = `${e.target.value }px`
 });
