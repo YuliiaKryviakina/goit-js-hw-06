cfa8dfe54ae6605369c10ba622d5aa893725b818
YuliiaKryviakina <117831702+YuliiaKryviakina@users.noreply.github.com>
2023-04-19 06:57:33 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,11 @@
+const formLogin = document.querySelector('.login-form');
 
+formLogin.addEventListener('submit', event => {
+ event.preventDefault();
+
+ const formData = {};
+ for (const element of formLogin.elements) {
+    if
+ }
+
+})
