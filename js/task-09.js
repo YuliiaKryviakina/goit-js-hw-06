7f1a4190cad7b88995500463214ee71d5d097400
YuliiaKryviakina <117831702+YuliiaKryviakina@users.noreply.github.com>
2023-04-19 23:40:20 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function changeBgColor() {
+  const color = getRandomHexColor();
+  document.body.style.background = color;
+  document.querySelector('.color').textContent = color;
+};
+
+const btn = document.querySelector('.change-color');
+addEventListener('click', changeBgColor);
